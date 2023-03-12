0e932d4af1953c017946c89f708e5fd818d3df61
Replit user <>
2023-03-12 01:38:33 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -88,9 +88,8 @@ typedef struct {
 
 Vector * crearVector(int n) {
    
-  
-  
-
+  vector * vectorTemp = (vector *) malloc(sizeof(vector));
+  if(p == NULL) return NULL;
   
 }
 
@@ -101,7 +100,7 @@ la cual asigna el valor a la posición i del vector v.
 */
 void asignarValor(Vector * v, int i, int valor) 
 {
-  v -> datos[i] = valor;
+  v->datos[i] = valor;
 }
 
 /*
